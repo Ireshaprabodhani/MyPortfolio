c6ed36370e6e9a011cf6699023f17a18c6667134
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2024-06-07 09:49:41 +0530

    remove blank

--- my-portfolio/src/components/Portfolio.jsx
+++ my-portfolio/src/components/Portfolio.jsx
@@ -53,7 +53,7 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <a href={href} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 cursor-pointer" target="_blank">
+                <a href={href} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 cursor-pointer">
                   Demo
                 </a>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
